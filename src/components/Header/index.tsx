80f806e597364f198373ba8cf399cb9d6bd3a9a1
markdavid000 <davidojukwu59@gmail.com>
2024-08-29 13:46:42 +0100

    state lists hooks, constants v1 factory and components header modified

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -131,7 +131,7 @@ const NETWORK_LABELS: { [chainId in ChainId]: string | null } = {
   [ChainId.RINKEBY]: 'Rinkeby',
   [ChainId.ROPSTEN]: 'Ropsten',
   [ChainId.GÖRLI]: 'Görli',
-  [ChainId.KOVAN]: 'Kovan'
+  [ChainId.KOVAN]: 'Kovan',
   [ChainId.BASE_SEPOLIA]: 'Base Sepolia'
 }
 
